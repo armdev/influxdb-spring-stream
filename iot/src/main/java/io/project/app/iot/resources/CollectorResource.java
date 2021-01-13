352ef7b8c7e42b206eaf60a59c59d6d6f3254dc2
root <root@YLT160001.corp.optym.net>
2021-01-13 23:16:24 +0400

    Next best push

--- iot/src/main/java/io/project/app/iot/resources/CollectorResource.java
+++ iot/src/main/java/io/project/app/iot/resources/CollectorResource.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package io.project.app.iot.resources;
 
 import io.project.app.iot.model.SensorMetrics;
